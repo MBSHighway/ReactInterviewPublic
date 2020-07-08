876a47367d390feb9b37357c9746500791f74db6
andrewfmbs <41008078+andrewfmbs@users.noreply.github.com>
2020-07-08 09:39:51 -0400

    Update BondCard.js

--- client/src/BondCard.js
+++ client/src/BondCard.js
@@ -4,10 +4,10 @@ import React from "react";
 const BondCard = ({ priceObj }) => {
 
     /**
-     * @param {float} timePrice price and time
+     * @param {float} timePrice price at given time
      * @param {float} currentPrice currentPrice
      * @returns {object} object containing absolute price difference between current and quote at time and direction.
-     * {price: xxx, direction: "up"}
+     * {price: 4, direction: "up"}
      */
     const createWindowObj = (timePrice, currentPrice) => {
         // Need to get change and direction here
@@ -25,6 +25,11 @@ const BondCard = ({ priceObj }) => {
     );
 };
 
+/** 
+* @param {int} price Price difference between now and the given time price.
+* @param {string} time Time window.
+* @param {string} direction "up" or "down".
+*/
 const PriceWindow = ({ price, time, direction }) => {
     return (
         <div style={{ padding: "25px", border: "1px solid grey", width: "200px" }} >

0ddf44f22b65d6f72d3642d69527630918d18c81
andrewfmbs <41008078+andrewfmbs@users.noreply.github.com>
2020-07-08 09:45:57 -0400

    Update BondCard.js

--- client/src/BondCard.js
+++ client/src/BondCard.js
@@ -4,6 +4,7 @@ import React from "react";
 const BondCard = ({ priceObj }) => {
 
     /**
+     * @function
      * @param {float} timePrice price at given time
      * @param {float} currentPrice currentPrice
      * @returns {object} object containing absolute price difference between current and quote at time and direction.
@@ -26,6 +27,7 @@ const BondCard = ({ priceObj }) => {
 };
 
 /** 
+* @component
 * @param {int} price Price difference between now and the given time price.
 * @param {string} time Time window.
 * @param {string} direction "up" or "down".
